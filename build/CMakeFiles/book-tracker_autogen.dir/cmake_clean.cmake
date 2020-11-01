42498182bce2e4d65f8b7089634b60dc1c2ba024
fainc <farincorperated@gmail.com>
2020-11-01 00:24:14 -0700

    Removed the mistakely added build files from theproject.

--- build/CMakeFiles/book-tracker_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/book-tracker_autogen"
-  "book-tracker_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/book-tracker_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
